940518be4afea44aba59be63c3a6d59b3b963a5e
phanhdanghoccode2005 <23001576@hus.edu.vn>
2025-05-10 03:58:47 -0400

    Thêm file thuhoc.py

---
+++ thuhoc.py
@@ -0,0 +1,12 @@
+def songuyento(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+so = int(input("nhap so: "))
+if so > 0 and songuyento(so):
+    print(f"{so} la so nguyen to.")
+else:
+    print(f"{so} ko phai so nguyen to.")
